a6e52f649e7389f6acea6df1c074f961f2359e62
Lovemanish <manish05453@gmail.com>
2025-01-26 13:40:58 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Array{
+    public static void main(String args[]){
+        int array[] = {1,2,4,5,6,7};
+        int sum = array[0]+1;
+        System.out.println(sum);
+    }
+    
+}
